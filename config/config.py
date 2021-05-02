562a68cc277cd758bc4ea751501f1ba4c66e8188
THE-MiniBEAST <julio.arenas@datazucar.cu>
2021-05-02 14:19:35 -0400

    Variable entorno directa

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ class Development(object):
     TESTING = False
     JWT_SECRET_KEY = 'ZG1WbWJHRjBMV3RwWlhKdkttTnZiRzl0WW1saEl6RXlNelExTmpjNE9UQWpNalZ0'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5433/prueba3'
-    SQLALCHEMY_DATABASE_URI = 'mysql://u406650721_marketing:Mnbv21.*@213.190.6.64/u406650721_dba24'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://u406650721_marketing:Mnbv21.*@213.190.6.64/u406650721_dba24'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SHOW_SQLALCHEMY_LOG_MESSAGES = False
     API_PROTOCOL = 'http://'
